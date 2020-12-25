ea929b0db42e792b605ea4a656a955dd6a83aa9e
Umair Shams <52600714+umairshams1991@users.noreply.github.com>
2020-12-25 17:46:03 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Dinner from './dinner.js';
 function App() {
   return (
     <div>
+      <h1>Hello World of Fame</h1>
      <Dinner disName = "Karahi" sweetDish = "kheer"/>
      <Dinner sweetDish = "Halwa" disName = "Baryani" />
     </div>
